24c33511b029696f9de5c0e4fc00a95bca97e903
Coul33t <Coulis1990@gmail.com>
2018-03-19 19:19:00 +0100

    Dunno lol

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pdb
 
 CONSOLE_SIZE = {'x': 80, 'y': 25}
 MAP_CONSOLE = {'x': 2, 'y': 2, 'w': 57, 'h': 21}
+TILES = {'wall': '#', 'floor': '.', 'door': '+'}
 
 class Tile(object):
     def __init__(self, char='.'):
@@ -51,7 +52,7 @@ def test_mouse():
     blt.set("input.filter={keyboard, mouse+}")
     blt.composition(True)
 
-    game_map = Map(10, 10)
+    game_map = Map(20, 10)
 
     precise_mouse = False
     cursor_visible = True
@@ -60,70 +61,34 @@ def test_mouse():
     scroll = 0
     plate = False
 
+    character = 'wall'
+
     # Flush input
     while blt.has_input():
         blt.read()
 
-    counter = 0
-
     proceed = True
+
     while proceed:
+
         blt.clear()
 
+        render(game_map)
+
         blt.color("white")
-        blt.puts(
-            1, 3,
-            "Buttons: "
-            "[color=%s]left "
-            "[color=%s]middle "
-            "[color=%s]right "
-            "[color=%s]x1 "
-            "[color=%s]x2 " % (
-                "orange" if blt.state(blt.TK_MOUSE_LEFT) else "dark gray",
-                "orange" if blt.state(blt.TK_MOUSE_MIDDLE) else "dark gray",
-                "orange" if blt.state(blt.TK_MOUSE_RIGHT) else "dark gray",
-                "orange" if blt.state(blt.TK_MOUSE_X1) else "dark gray",
-                "orange" if blt.state(blt.TK_MOUSE_X2) else "dark gray"))
-
-
-        blt.puts(
-            1, 5,
-            "Wheel: [color=orange]%d[/color] [color=dark gray]delta[/color]"
-            ", [color=orange]%d[/color] [color=dark gray]cumulative" % (
-                blt.state(blt.TK_MOUSE_WHEEL), scroll))
-
-        blt.puts(double_click_area.x, double_click_area.y - 1, "Double-click here:")
-        blt.color("darker orange" if plate else "darker gray")
-        double_click_area.fill()
 
         if blt.state(blt.TK_MOUSE_LEFT) and is_in_map_console():
             x = blt.state(blt.TK_MOUSE_X)-2
             y = blt.state(blt.TK_MOUSE_Y)-2
             if x >= 0 and x < game_map.w and y >= 0 and y < game_map.h:
-                if game_map.get_tile(x,y).char == '.':
-                    game_map.get_tile(x,y).char = '#'
-                elif game_map.get_tile(x,y).char == '#':
-                    game_map.get_tile(x,y).char = '.'
+                game_map.get_tile(x,y).char = TILES[character]
 
-        render(game_map)
-
-        mx = blt.state(blt.TK_MOUSE_X)
-        my = blt.state(blt.TK_MOUSE_Y)
-        blt.color(0x60FFFFFF)
-        for x in range(blt.state(blt.TK_WIDTH)): blt.put(x, my, 0x2588)
-        for y in range(blt.state(blt.TK_HEIGHT)): blt.put(mx, y, 0x2588)
-
-        blt.color(0x8000FF00)
-        blt.put(mlx, mly, 0x2588)
-
-        blt.color(0x80FF00FF)
-        blt.put(mrx, mry, 0x2588)
+        blt.put(blt.state(blt.TK_MOUSE_X), blt.state(blt.TK_MOUSE_Y), 0x2588)
 
         blt.refresh()
 
         while True:
             code = blt.read()
-            counter += 1
 
             if code in (blt.TK_ESCAPE, blt.TK_CLOSE):
                 proceed = False
@@ -132,20 +97,10 @@ def test_mouse():
                 x = blt.state(blt.TK_MOUSE_X)
                 y = blt.state(blt.TK_MOUSE_Y)
 
-                if x == 1 and (y == 7 or y == 8):
-                    if y == 7:
-                        precise_mouse = not precise_mouse
-                        blt.set("input.precise-mouse=%s" % ("true" if precise_mouse else "false"))
-                    else:
-                        cursor_visible = not cursor_visible
-                        blt.set("input.mouse-cursor=%s" % ("true" if cursor_visible else "false"))
-                elif (x,y) in double_click_area:
-                    clicks = blt.state(blt.TK_MOUSE_CLICKS)
-                    if clicks > 0 and clicks % 2 == 0:
-                        plate = not plate
-                else:
-                    mlx = x
-                    mly = y
+                if x > 60 and 3 <= y <= 3 + len(TILES.keys()):
+                    character = list(TILES.keys())[y-3]
+                
+                
 
             elif code == blt.TK_MOUSE_RIGHT:
                 mrx = blt.state(blt.TK_MOUSE_X)
@@ -166,9 +121,9 @@ def test_mouse():
 
 
 def is_in_map_console():
-    if (blt.state(blt.TK_MOUSE_X) > MAP_CONSOLE['x'] and
+    if (blt.state(blt.TK_MOUSE_X) > MAP_CONSOLE['x'] - 1 and
         blt.state(blt.TK_MOUSE_X) < MAP_CONSOLE['x'] + MAP_CONSOLE['w'] and
-        blt.state(blt.TK_MOUSE_Y) > MAP_CONSOLE['y'] and
+        blt.state(blt.TK_MOUSE_Y) > MAP_CONSOLE['y'] - 1 and
         blt.state(blt.TK_MOUSE_Y) < MAP_CONSOLE['y'] + MAP_CONSOLE['h']):
 
         return True
@@ -191,15 +146,28 @@ def render(game_map):
     for y in range(25):
         blt.puts(1, y, "║")
         blt.puts(60, y, "║")
-    # Then the scrolling bar
+    # Then the left scrolling bar
     for y in range(MAP_CONSOLE['y'], MAP_CONSOLE['y'] + MAP_CONSOLE['h']):
         blt.puts(0, y, "░")
 
     # For now, it's a placeholder
     blt.puts(0, 2, "▓")
 
+    # Then the bottom scrolling bar
+    for x in range(MAP_CONSOLE['x'], MAP_CONSOLE['x'] + MAP_CONSOLE['w']):
+        blt.puts(x, CONSOLE_SIZE['y']-1, "░")
+
+    # For now, it's a placeholder
+    blt.puts(2, CONSOLE_SIZE['y']-1, "▓")
+
     blt.color("white")
-    blt.puts(61, 2, f"Cursor: [color=orange]{blt.state(blt.TK_MOUSE_X)}:{blt.state(blt.TK_MOUSE_Y)}[/color]")
+    # Right panel display
+    # Cursor position
+    blt.puts(61, 2, f"Cursor: [color=orange]{blt.state(blt.TK_MOUSE_X)-2}:{blt.state(blt.TK_MOUSE_Y)-2}[/color]")
+
+    for y,(key, val) in enumerate(TILES.items()):
+        blt.put(61, y+3, 0x25CB)
+        blt.puts(63, y+3, f"{key}")
 
     for x in range(game_map.w):
         for y in  range(game_map.h):
